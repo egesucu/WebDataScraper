59f305ce004598df92f97de7dd4dcd0ccbc0377f
Ege Sucu <egesucu@icloud.com>
2022-05-06 17:02:19 +0300

    Initial Commit

---
+++ WebDataScraper/WebDataScraperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebDataScraperApp.swift
+//  WebDataScraper
+//
+//  Created by Ege Sucu on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WebDataScraperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
